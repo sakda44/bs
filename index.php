7b5e43cf85b3f6dd3a915b7f0006668a28c3a7db
sakda44 <86420617+sakda44@users.noreply.github.com>
2021-12-09 13:57:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,194 @@
+<!doctype html>
+<html lang="en">
 
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+
+    <title>Sakda_63110282</title>
+    <style>
+        .bg {
+            background-color: rgb(218, 210, 199);
+        }
+
+        .bg0 {
+            background-color: blanchedalmond;
+        }
+
+        .bg1 {
+            background-color: rgb(207, 174, 124);
+        }
+
+        .bg2 {
+            background-color: rgb(230, 173, 88);
+        }
+
+        .bg3 {
+            background-color: rgb(148, 109, 50);
+        }
+    </style>
+</head>
+
+<body class="bg">
+    <div class="container">
+        <div>
+            <div class="col-12 bg0">
+                <img src="https://www.smeleader.com/wp-content/uploads/2020/03/%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B8%82%E0%B8%99%E0%B8%A1-%E0%B8%95%E0%B8%B1%E0%B9%89%E0%B8%87%E0%B8%8A%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%9A%E0%B9%80%E0%B8%81%E0%B8%AD%E0%B8%A3%E0%B8%B5%E0%B9%88-660x330.jpg"
+                    class="rounded mx-auto d-block" alt=" ">
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-4 bg1">
+
+            </div>
+            <br>
+            <div class="col-8 bg2">
+                <div class="row">
+                    <div class="col-6 ">
+                        <picture>
+                            <source
+                                srcset="https://s359.kapook.com/pagebuilder/735689c8-45f3-43a7-a4e4-762352982580.jpg"
+                                type="image/svg+xml">
+                            <img src="https://s359.kapook.com/pagebuilder/735689c8-45f3-43a7-a4e4-762352982580.jpg"
+                                class="img-fluid img-thumbnail" alt=" ">
+                        </picture>
+                        <p class="card-text">เบเกอรี่เริ่มมีขึ้นในยุคหิน
+                            โดยชาวสวิสได้เป็นผู้ริเริ่มนำเมล็ดข้าวสาลีมาบดให้แตก ผสมน้ำ ทำให้สุกบนแผ่นหินเผาไฟ
+                            ได้อาหารเป็นแผ่น ข้างในเหนียวเหนอะหนะ นับเป็นขนมปังชนิดแรกของโลกเลยค่ะ และต่อมา
+                            ชาวอียิปต์ได้พัฒนาจากขนมปังที่เป็นก้อนแน่น ให้มาเป็นก้นโปร่งฟูขึ้น
+                            ซึ่งมาจากที่ชาวอียิปต์หมักก้อนแป้งแล้วลืมทิ้งไว้
+                            และได้นำมาผสมกับแป้งที่ทำใหม่เพื่อให้ขนมขึ้นฟู
+                            และชาวอียิปต์ยังได้นำดินเหนียวมาทำเป็นภาชนะเพื่อใช้ในการอบขนมแทนแผ่นหิน
+                            ซึ่งนั่นก็ถือว่าเป็นเตาอบชนิดแรกของโลก และเตาอบชนิดนี้ แบ่งเป็น 2 ชั้น คือ ชั้นล่างไว้ก่อไฟ
+                            ชั้นบนสำหรับอบขนม เชื่อกันว่าเริ่มมีการผลิตมาตั้งแต่ ๓,๐๐๐ ปีก่อน
+                            โดยต้นกำเนิดมาจากพวกทาสในยุคอียิปต์โบราณ
+                            ที่ได้ผสมก้อนแป้งที่ลืมทิ้งไว้ลงในแป้งที่ผสมเสร็จใหม่ๆ
+                            ทำให้เกิดผลิตภัณฑ์ที่มีลักษณะคล้ายขนมปัง คือเนื้อแป้งเบา ฟู และมีรสชาติอร่อย
+                            หลังจากนำไปทำให้สุก </p>
+                    </div>
+                    <div class="col-6 ">
+                        <picture>
+                            <source
+                                srcset="https://www.ryt9.com/img/files/20190226/iq8f8507d0b550368320ff0c887dabda26-0.jpg"
+                                type="image/svg+xml">
+                            <img src="https://www.ryt9.com/img/files/20190226/iq8f8507d0b550368320ff0c887dabda26-0.jpg"
+                                class="img-fluid img-thumbnail" alt="...">
+                        </picture>
+                        <p class="card-text">ในสมัยกรีกได้พัฒนาการทำขนมปัง โดยปั้นเป็นก้อนกลมรี น้ำหนักก้อนละ 1 ปอนด์
+                            และเปลี่ยนรูปแบบเตาอบเป็นลักษณะคล้ายรวงผื้ง ซื่งยังใช้ไม้เป็นเชื้อเพลิงเหมือนเดิม
+                            ต่อมาในสมัยโรมัน ก็มีการพัฒนาเทคโนโลยีการทำขนมปังเพิ่มขึ้น
+                            โดยสร้างเครื่องผสมซึ่งประกอบด้วยอ่างหินและพายไม้ และก็พัฒนามาเรื่อย จนถึงศตวรรษที่ 13
+                            ชาวฝรั่งเศสได้บันทึกถึงความก้าวหน้าของการพัฒนาเครื่องทำขนม ชนิดของขนมปัง
+                            และเมื่อกลางศตวรรษที่ 20 เป็นต้นมาอุตสาหกรรมขนมอบก็เริ่มเกิดขึ้น และมีการพัฒนาต่อเนื่อง</p>
+                    </div>
+                    <div class="col-12 bg0">
+                        <button id="btnBack"> back </button>
+                        <div id="main">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th> Details </th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tblPosts">
+                                </tbody>
+                            </table>
+                        </div>
+                        <div id="detail">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Title</th>
+                                        <th>Author</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="tbldetail">
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div>
+            <div class="col-12 bg0 center">
+                <h1>ศักดิ์ดา เอี่ยมพงศ์ษา 63110282</h1>
+            </div>
+        </div>
+
+
+    </div>
+
+
+</body>
+<script>
+    function showDetails(id){
+        $("#main").hide();
+        $("#detail").show();
+        var url = "https://jsonplaceholder.typicode.com/posts/"+id
+        $.getJSON(url)
+            .done((data)=>{
+                console.log(data);                    
+                var line = "<tr id='detail_title'";
+                    line += "><td>" + data.id + "</td>"
+                    line += "<td><b>" + data.title + "</b><br/>"
+                    line += data.body + "</td>"
+                    line += "<td>" + data.userId + "</td>"
+                    line += "</tr>";
+                    $("#tbldetail").append(line);
+            })
+            .fail((xhr, err, status) => {
+
+            })
+    }
+    function loadPosts(){
+        $("#main").show();
+        $("#detail").hide();
+        
+        var url = "https://jsonplaceholder.typicode.com/posts";
+        var i=0;
+        $.getJSON(url)
+            .done((data)=>{
+                $.each(data, (k, item)=>{
+                    //console.log(item);
+                    i++;
+                    var line = "<tr>";
+                        line += "<td>"+ item.id + "</td>";
+                        line += "<td><b>"+ item.title + "</b><br/>";
+                        line += item.body + "</td>";
+                        line += "<td> <button onClick='showDetails("+ item.id +");' > link </button> </td>";
+                        line += "</tr>";
+                    $("#tblPosts").append(line); 
+                    if (i==10){
+                        loadPost().stop();
+                    };
+                });
+                $("#main").show();
+            })
+            .fail((xhr, status, error)=>{
+            })
+    }
+    $(()=>{
+        loadPosts();
+        $("#btnBack").click(() => {
+            $("#detail_title").remove();
+            $("#detail").hide();
+            $("#main").show();
+        $("#detail").hide();
+        });
+    })
+</script>
+</html>
